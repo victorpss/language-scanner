37d6583b2ce0e678707f80afac9126226ca7a7d6
victorpss <victorpss@dcc.ufrj.br>
2025-01-12 15:41:23 -0300

    feat: homepage

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className='home-container'>
+            <div className='home-title'>
+                <p>Language scanner</p>
+            </div>
+            <div className='input-output'>
+                <div className='input-container'>
+                    <textarea className='input-field' placeholder='Start your text here...' type='text'></textarea>
+                </div>
+                <div className='output-container'>
+                    <div>
+                        <p className='output-title'>Predicted languages:</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
